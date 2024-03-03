5085e5bc47ed27ff4c43972ed8bc2fb200e911a5
EJ-Lan <ervinjon_lanada@outlook.com>
2024-03-03 08:08:20 -0700

    Add spring initializer files

---
+++ campuscause/src/main/java/com/codewithej/campuscause/CampuscauseApplication.java
@@ -0,0 +1,13 @@
+package com.codewithej.campuscause;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampuscauseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampuscauseApplication.class, args);
+	}
+
+}
---
+++ campuscause/src/test/java/com/codewithej/campuscause/CampuscauseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithej.campuscause;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampuscauseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
