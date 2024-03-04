b2449b5f1115c37f711fdf2ea899eb8ba74bfdc4
EJ-Lan <ervinjon_lanada@outlook.com>
2024-03-03 21:55:11 -0700

    Comment entity

--- backend/campuscause/src/main/java/com/codewithej/campuscause/entity/Comment.java
+++ backend/campuscause/src/main/java/com/codewithej/campuscause/entity/Comment.java
@@ -1,4 +1,29 @@
 package com.codewithej.campuscause.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Document(collection = "comments")
 public class Comment {
+
+    @Id
+    private String id;
+
+    @DBRef
+    Project project;
+
+    @DBRef
+    User author;
+
+    private String content;
+    private Date datePosted;
 }
