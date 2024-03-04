96aeed9be3440caadac3ef48f9e4509b104b6f92
EJ-Lan <ervinjon_lanada@outlook.com>
2024-03-03 21:37:44 -0700

    Embedded update entity

--- backend/campuscause/src/main/java/com/codewithej/campuscause/entity/Update.java
+++ backend/campuscause/src/main/java/com/codewithej/campuscause/entity/Update.java
@@ -1,4 +1,23 @@
 package com.codewithej.campuscause.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Update {
+
+    @Id
+    private String id;
+
+    private String title;
+    private String content;
+    private Date datePosted;
 }
