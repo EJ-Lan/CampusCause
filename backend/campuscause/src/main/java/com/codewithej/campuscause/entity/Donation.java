bd29f73e054e51aa10e8750f6ba4420ea4e64bc5
EJ-Lan <ervinjon_lanada@outlook.com>
2024-03-03 21:54:04 -0700

    Entity for donation

--- backend/campuscause/src/main/java/com/codewithej/campuscause/entity/Donation.java
+++ backend/campuscause/src/main/java/com/codewithej/campuscause/entity/Donation.java
@@ -1,4 +1,29 @@
 package com.codewithej.campuscause.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Document(collection = "donations")
 public class Donation {
+
+    @Id
+    private String id;
+
+    @DBRef
+    private Project project;
+
+    @DBRef
+    private User donor;
+
+    private double amount;
+    private Date date;
 }
